7349487cec5f144415cf4d026b8aded65165c9dc
kenzan8000 <kenzan8000@gmail.com>
2017-06-12 22:23:28 -0700

    renew project

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,3 @@
 Rails.application.routes.draw do
-
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users, :controllers => {
-    :sessions      => "users/sessions",
-    :registrations => "users/registrations",
-    :passwords     => "users/passwords",
-    :omniauth_callbacks => "users/omniauth_callbacks"
-  }
-
-  root to: 'home#get_index'
-
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
